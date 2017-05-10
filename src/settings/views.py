acf98f8c6d984ec2814ab41f5bf7a81811e43fd7
Omar Lozada <omrlozada@gmail.com>
2017-05-11 04:13:21 +0800

    Settings App (#1)
    * Add settings app to admin
    
    * Include settings urls.py
    
    * Add settings app
    
    * Use github gitignore too
    
    * Add Settings app migration 001 (Project model)
    
    * remove __init__.pyc
    
    * Rename database to ansible_dev
    
    * Rename database to ansible_dev
    
    * Additional fields on Project
    
    * Migration for additional fields of Project model
    
    * Set default path as '~/'
    
    * Alter migration to set defaul path as '~/'

---
+++ src/settings/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponseRedirect, HttpResponse
+
+from .models import Project
+
+def index(request):
+    return "200"
