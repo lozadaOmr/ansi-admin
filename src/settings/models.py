a07be94adb909b234715f3614c3bddcf628241df
Omar Lozada <omrlozada@gmail.com>
2017-05-11 03:42:14 +0800

    Additional fields on Project

--- src/settings/models.py
+++ src/settings/models.py
@@ -2,4 +2,7 @@ from django.db import models
 
 class Project(models.Model):
     project_name = models.CharField(max_length=200)
-    playbook_path = models.CharField(max_length=200)
+    playbook_path = models.CharField(max_length=200, default="~")
+    ansible_config_path = models.CharField(max_length=200, default="~")
+    default_inventory = models.CharField(max_length=200, default="hosts")
+    default_user = models.CharField(max_length=200, default="ubuntu")
