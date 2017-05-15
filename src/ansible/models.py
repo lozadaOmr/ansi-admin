c9cb05bdf65178c350763b81cb535229ace9cc45
Omar Lozada <omrlozada@gmail.com>
2017-05-16 03:16:55 +0800

    Rewrite everything into one app

--- src/ansible/models.py
+++ src/ansible/models.py
@@ -1,24 +1,15 @@
 from django.db import models
 
-class Project(models.Model):
-    project_name = models.CharField(max_length=200)
-    playbook_path = models.CharField(max_length=200, default="~/")
-    ansible_config_path = models.CharField(max_length=200, default="~/")
-    default_inventory = models.CharField(max_length=200, default="hosts")
-    default_user = models.CharField(max_length=200, default="ubuntu")
+class Playbook(models.Model):
 
-
-class Registry(models.Model):
     class Meta:
-        verbose_name_plural = "registries"
+        verbose_name_plural = "playbooks"
 
-
-    project = models.OneToOneField(
-        Project,
-        on_delete=models.CASCADE,
-        primary_key=True,
-    )
     name = models.CharField(max_length=200)
+    path = models.CharField(max_length=200, default="~/")
+    ansible_config = models.CharField(max_length=200, default="~/")
+    inventory = models.CharField(max_length=200, default="hosts")
+    user = models.CharField(max_length=200, default="ubuntu")
 
     def __str__(self):
-        return "project name: %s" % self.project.project_name
+        return "Playbook name: %s" % self.playbook.name
--- src/web/settings.py
+++ src/web/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'ansible.apps.SettingsConfig',
+    'ansible.apps.AnsibleConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
