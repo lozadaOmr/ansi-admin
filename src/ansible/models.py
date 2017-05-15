7fda64e9d093664eead888d5712d15f7ca8e8440
Omar Lozada <omrlozada@gmail.com>
2017-05-16 05:18:47 +0800

    Add Registry class

--- src/ansible/models.py
+++ src/ansible/models.py
@@ -14,3 +14,15 @@ class Playbook(models.Model):
 
     class Meta:
         verbose_name_plural = "playbooks"
+
+
+class Registry(models.Model):
+    playbook = models.ForeignKey(Playbook, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    item = models.FilePathField(path=settings.PLAYBOOK_DIR, recursive=True)
+
+    def __str__(self):
+        return "%s" % self.name
+
+    class Meta:
+        verbose_name_plural = "registries"
