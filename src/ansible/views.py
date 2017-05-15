8cfa8f4dce1fe6810be45c08b9a26126db81fa3a
Omar Lozada <omrlozada@gmail.com>
2017-05-16 03:16:55 +0800

    Rewrite everything into one app

--- src/ansible/views.py
+++ src/ansible/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect, HttpResponse
 
-from .models import Project
+from .models import Playbook
 
 def index(request):
     return "200"
