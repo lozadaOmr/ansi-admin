582d76e0ac7fadfa81420d8c02c155c24be2e567
Omar Lozada <omrlozada@gmail.com>
2017-05-16 22:34:43 +0800

    Add hidden field 'directory'
    Format from Playbook.name

--- src/ansible/models.py
+++ src/ansible/models.py
@@ -2,15 +2,22 @@ from django.db import models
 from django.conf import settings
 
 class Playbook(models.Model):
-
-    default_path = settings.PLAYBOOK_DIR
-
     name = models.CharField(max_length=200)
     inventory = models.CharField(max_length=200, default="hosts")
     user = models.CharField(max_length=200, default="ubuntu")
+    directory = models.CharField(max_length=200, editable=False, default="dir")
 
     def __str__(self):
         return "%s" % self.name
+
+    def format_directory(self):
+        directory = self.name.lower()
+        directory = directory.replace(" ","-")
+        return directory
+
+    def save(self, *args, **kwargs):
+        self.directory = self.format_directory()
+        super(Playbook, self).save(*args, **kwargs)
 
     class Meta:
         verbose_name_plural = "playbooks"
