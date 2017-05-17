b9cf39e1e872e39fafe00d659e7c058a6ac0b6e9
Omar Lozada <omrlozada@gmail.com>
2017-05-18 05:11:02 +0800

    Change ansible.urls from 'settings' to 'ansible'

--- src/web/urls.py
+++ src/web/urls.py
@@ -3,5 +3,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^settings/', include('ansible.urls')),
+    url(r'^ansible/', include('ansible.urls')),
 ]
