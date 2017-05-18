a9fcdca804dc7cea27aa0fc47ea6341a687d88d2
Omar Lozada <omrlozada@gmail.com>
2017-05-18 23:24:08 +0800

    Add Repository in imports

--- src/ansible/admin.py
+++ src/ansible/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Playbook, Registry
+from .models import Playbook, Registry, Repository
 
 admin.site.register(Playbook)
 admin.site.register(Registry)
