d1d32cb42b56797e917b911f949ba96fb9f4d0b9
Omar Lozada <omrlozada@gmail.com>
2017-05-20 03:05:30 +0800

    Add Repository app in admin settings

--- src/web/settings.py
+++ src/web/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'ansible.apps.AnsibleConfig',
+    'repository.apps.RepositoryConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
