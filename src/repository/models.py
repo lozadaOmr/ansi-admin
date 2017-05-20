4a7dd8da7f774df0166a1b9d43b075afd99c1fa3
Omar Lozada <omrlozada@gmail.com>
2017-05-20 23:31:35 +0800

    Clone repository on save

--- src/repository/models.py
+++ src/repository/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.conf import settings
+import git, os
 
 class Github (models.Model):
     username = models.CharField(max_length=39)
@@ -7,6 +7,21 @@ class Github (models.Model):
 
     def __str__(self):
         return self.repository
+
+    def clone_repository(self):
+        DIR_NAME = self.repository
+        REMOTE_URL = "https://github.com/{0}/{1}.git".format(self.username, self.repository)
+
+        os.mkdir(DIR_NAME)
+
+        repo = git.Repo.init(DIR_NAME)
+        origin = repo.create_remote('origin', REMOTE_URL)
+        origin.fetch()
+        origin.pull(origin.refs[0].remote_head)
+
+    def save(self, *args, **kwargs):
+        self.clone_repository()
+        super(Github, self).save(*args, **kwargs)
 
     class Meta:
         verbose_name_plural = "projects"
