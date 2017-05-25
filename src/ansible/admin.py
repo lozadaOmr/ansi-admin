95655678878540108c82ac5d2eb7c21386af0e45
Omar Lozada <omrlozada@gmail.com>
2017-05-25 20:44:27 +0800

    Remove Repository model

--- src/ansible/admin.py
+++ src/ansible/admin.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
 
-from .models import Playbook, Registry, Repository
+from .models import Playbook, Registry
 
 admin.site.register(Playbook)
 admin.site.register(Registry)
-admin.site.register(Repository)
 admin.site.site_header = 'Ansible Admin'
 admin.site.site_title = 'Ansible Admin'
 admin.site.index_title = 'Admin Tool'
