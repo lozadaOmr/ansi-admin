2e2d3d9b3fc8e3208839f4b6671942cc39297bdf
Omar Lozada <omrlozada@gmail.com>
2017-06-03 23:34:20 +0800

    Delete Repository App

--- src/repository/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-from .models import Github, Gitlab
-
-admin.site.register(Github)
-admin.site.register(Gitlab)
--- src/repository/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-
-class ProjectForm(forms.ModelForm):
-    class Meta:
-        model = Project 
-        widgets = {
-        'password': forms.PasswordInput(),
-    }
