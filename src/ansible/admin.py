65383464938bf445f72a5accdbc03d53a7f5f8e8
Omar Lozada <omrlozada@gmail.com>
2017-06-03 23:30:22 +0800

    Register Github and Gitlab in Ansible model

--- src/ansible/admin.py
+++ src/ansible/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 
-from .models import Playbook, Registry
+from .models import Github, Gitlab, Playbook, Registry
 
 admin.site.register(Playbook)
 admin.site.register(Registry)
+admin.site.register(Github)
+admin.site.register(Gitlab)
 admin.site.site_header = 'Ansible Admin'
 admin.site.site_title = 'Ansible Admin'
 admin.site.index_title = 'Admin Tool'
