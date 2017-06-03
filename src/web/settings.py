fbb7dce805e30bd88b0aa34df24ef47ecbd6a64b
Omar Lozada <omrlozada@gmail.com>
2017-06-03 23:31:47 +0800

    Remove RepositoryConfig in settings config

--- src/web/settings.py
+++ src/web/settings.py
@@ -32,7 +32,6 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'ansible.apps.AnsibleConfig',
-    'repository.apps.RepositoryConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
