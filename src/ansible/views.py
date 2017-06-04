e5f6f24ec436498d6693e445b76a18e981ff3925
Omar Lozada <omrlozada@gmail.com>
2017-06-04 23:34:32 +0800

    Add custom create form for Ansible Repository

--- src/ansible/views.py
+++ src/ansible/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect, HttpResponse
 
-from .models import Playbook
+from .models import Github, Gitlab, Playbook
+from .forms import RepositoryForm
 
 def index(request):
     return HttpResponse("200")
+
+def create(request):
+    form = RepositoryForm()
+    return render(request, 'ansible/create.html', {'form': form})
+
