90c5f8c700d4de32eb1532ef34e7566b4b2833fa
Omar Lozada <omrlozada@gmail.com>
2017-06-06 21:57:36 +0800

    Rename URL as 'add_playbook'

--- src/ansible/urls.py
+++ src/ansible/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^create$', views.create, name='create'),
+    url(r'^create$', views.create, name='add_playbook'),
 ]
