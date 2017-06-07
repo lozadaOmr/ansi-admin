01d54e6b9c5da6d1e605807bf4ddfd064c11acad
Omar Lozada <omrlozada@gmail.com>
2017-06-07 23:16:23 +0800

    Working views

--- src/ansible/views.py
+++ src/ansible/views.py
@@ -1,13 +1,23 @@
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect, HttpResponse
 
-from .models import Github, Gitlab, Playbook
 from .forms import RepositoryForm
+from ansible.models import Github
 
 def index(request):
     return HttpResponse("200")
 
 def create(request):
-    form = RepositoryForm()
+    if request.method == 'POST':
+        form = RepositoryForm(request.POST)
+        if form.is_valid():
+            project = Github()
+            project.repository = form.cleaned_data['repository']
+            project.username = form.cleaned_data['username']
+            project.save()
+            # Redirect here for now
+            return HttpResponseRedirect('create')
+    else:
+        form = RepositoryForm()
     return render(request, 'ansible/create.html', {'form': form})
 
