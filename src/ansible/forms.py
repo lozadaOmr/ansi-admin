5ae1dd5cb9a1d161d1b4fb06bf550b5847446a24
Omar Lozada <omrlozada@gmail.com>
2017-06-08 18:44:42 +0800

    Update form classnames

--- src/ansible/forms.py
+++ src/ansible/forms.py
@@ -1,6 +1,12 @@
 from django import forms
 
 
-class RepositoryForm(forms.Form):
+class AnsibleForm1(forms.Form):
     repository = forms.CharField(label='Repository', max_length=100)
     username = forms.CharField(label='Username', max_length=39)
+
+class AnsibleForm2(forms.Form):
+    name = forms.CharField(label='Name', max_length=200)
+    inventory = forms.CharField(label='Inventory', max_length=200)
+    user = forms.CharField(label='Remote User', max_length=200)
+
