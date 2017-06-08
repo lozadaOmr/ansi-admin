8a849183fc53ee372ca1d5c1b844219ffe642670
Omar Lozada <omrlozada@gmail.com>
2017-06-08 17:58:17 +0800

    Install django 'formtools'

--- src/web/settings.py
+++ src/web/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'ansible.apps.AnsibleConfig',
+    'formtools',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
