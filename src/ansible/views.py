a82939dc698dc2b79b64d1a7436281d5d021f0db
Omar Lozada <omrlozada@gmail.com>
2017-06-10 23:31:27 +0800

    Save Form Wizard data
    For now AnsibleForm1 stored as github model
    Save AnsibleForm2 in as playbook model

--- src/ansible/views.py
+++ src/ansible/views.py
@@ -7,22 +7,19 @@ def index(request):
     return HttpResponse("200")
 
 class PlaybookWizard(SessionWizardView):
-    instance = None
-
-    def get_form_instance(self, step):
-        if self.instance is None:
-            self.instance = Github()
-        return self.instance
-
     def get_form_initial(self, step):
         initial = {}
-
         if step == '1':
             prev_data = self.storage.get_step_data('0')
             initial['name'] = prev_data['0-repository']
             return self.initial_dict.get(step, initial)
+
         return self.initial_dict.get(step, {})
 
 
     def done(self, form_list, **kwargs):
-        return HttpResponseRedirect('ansible/create')
+        form_data = {}
+        for form in form_list:
+            form.save()
+
+        return HttpResponseRedirect('/ansible')
