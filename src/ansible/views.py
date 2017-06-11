31e4da5e782c29d7d0c893a3fc9af48260c50a3a
Omar Lozada <omrlozada@gmail.com>
2017-06-11 23:09:46 +0800

    Save form data to DB on each step

--- src/ansible/views.py
+++ src/ansible/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect, HttpResponse
 from formtools.wizard.views import SessionWizardView
-from ansible.models import Github
+from ansible.models import Github, Playbook
+import sys
+
 
 def index(request):
     return HttpResponse("200")
+
 
 class PlaybookWizard(SessionWizardView):
     def get_form_initial(self, step):
@@ -17,9 +20,24 @@ class PlaybookWizard(SessionWizardView):
         return self.initial_dict.get(step, {})
 
 
-    def done(self, form_list, **kwargs):
-        form_data = {}
-        for form in form_list:
-            form.save()
+    def get_form_step_data(self, form):
+        data = {}
+        if self.get_form_prefix() == '0':
+            github = Github()
+            github.repository = form.data.dict()['0-repository']
+            github.username = form.data.dict()['0-username']
+            github.save()
 
+        if self.get_form_prefix() == '1':
+            playbook = Playbook()
+            playbook.name = form.data.dict()['1-name']
+            playbook.inventory = form.data.dict()['1-inventory']
+            playbook.user = form.data.dict()['1-user']
+            playbook.save()
+
+        return form.data
+
+
+    def done(self, form_list, **kwargs):
         return HttpResponseRedirect('/ansible')
+
