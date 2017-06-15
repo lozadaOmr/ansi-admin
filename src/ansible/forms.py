cabd33bad2d86a42c531643ef95b7b59975a20d4
Omar Lozada <omrlozada@gmail.com>
2017-06-15 23:58:18 +0800

    Try Raise validation error on forms.py

--- src/ansible/forms.py
+++ src/ansible/forms.py
@@ -1,10 +1,16 @@
 from django.forms import ModelForm
+from django.core.validators import ValidationError
 from ansible.models import Github, Playbook
 
 class AnsibleForm1(ModelForm):
     class Meta:
         model = Github
         fields = ['repository', 'username']
+
+    def clean_repository(self):
+        if self.instance.check_repository_exists():
+            raise ValidationError('Repository directory already exist')
+        return self.cleaned_data
 
 class AnsibleForm2(ModelForm):
     class Meta:
