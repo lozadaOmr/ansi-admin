f719f1895aadd086c35338c2ae0d926784d835c7
Omar Lozada <omrlozada@gmail.com>
2017-06-17 19:20:56 +0800

    Move method from models to forms

--- src/ansible/forms.py
+++ src/ansible/forms.py
@@ -1,6 +1,8 @@
 from django.forms import ModelForm
 from django.core.validators import ValidationError
 from ansible.models import Github, Playbook
+from django.conf import settings
+import os
 
 class AnsibleForm1(ModelForm):
     class Meta:
@@ -8,9 +10,13 @@ class AnsibleForm1(ModelForm):
         fields = ['repository', 'username']
 
     def clean_repository(self):
-        if self.instance.check_repository_exists():
+        # TODO: move validation to validators.py
+        if self.check_repository_exists():
             raise ValidationError('Repository directory already exist')
         return self.cleaned_data
+
+    def check_repository_exists(self):
+        return os.path.exists(os.path.join(settings.PLAYBOOK_DIR, self.cleaned_data['repository']))
 
 class AnsibleForm2(ModelForm):
     class Meta:
