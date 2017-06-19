f9b049bd3561ecdff5db7cd62a0b4b010a3fd25c
Omar Lozada <omrlozada@gmail.com>
2017-06-19 23:34:52 +0800

    Use SessionWizardView

--- src/ansible/views.py
+++ src/ansible/views.py
@@ -1,19 +1,20 @@
 from django.core.urlresolvers import reverse
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect, HttpResponse
-from formtools.wizard.views import NamedUrlSessionWizardView
+from formtools.wizard.views import SessionWizardView
 from ansible.models import Playbook
-
 
 def index(request):
     return HttpResponse("200")
 
 
-class PlaybookWizard(NamedUrlSessionWizardView):
+class PlaybookWizard(SessionWizardView):
     def get_form_initial(self, step):
         initial = {}
         return self.initial_dict.get(step, {})
 
-    def done(self, form_list, **kwargs):
+    def done(self, form_list, form_dict,**kwargs):
+        for form in form_list:
+            form.save()
         return HttpResponseRedirect('/playbooks')
 
