7f8bd2fa80cd7bf440f48084a4551f9f3c843b94
Omar Lozada <omrlozada@gmail.com>
2017-06-19 23:34:52 +0800

    Use SessionWizardView

--- src/ansible/urls.py
+++ src/ansible/urls.py
@@ -4,16 +4,7 @@ from . import views
 from ansible.forms import AnsibleForm1, AnsibleForm2
 from ansible.views import PlaybookWizard
 
-
-named_forms = (
-    ('1', AnsibleForm1),
-    ('2', AnsibleForm2),
-)
-playbook_wizard = PlaybookWizard.as_view(
-    named_forms,
-    url_name='createstep',
-)
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^create/(?P<step>.+)/$', playbook_wizard, name='createstep')
+    url(r'^create/$', PlaybookWizard.as_view([AnsibleForm1, AnsibleForm2]))
 ]
