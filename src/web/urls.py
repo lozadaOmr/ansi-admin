a00704a5d0ee2e8cd188f06cd686b3f3e471654e
Omar Lozada <omrlozada@gmail.com>
2017-06-27 14:38:54 +0800

    Redirect authenticated users

--- src/web/urls.py
+++ src/web/urls.py
@@ -4,8 +4,9 @@ from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
-    url(r'^login/', auth_views.login, name='login'),
-    url(r'^logout/', auth_views.logout, {'next_page': '/'}, name='logout'),
+    url(r'^login/', auth_views.login, name='login',
+        kwargs={'redirect_authenticated_user': True}),
+    url(r'^logout/', auth_views.logout, {'next_page': '/login'}, name='logout'),
     url(r'^admin/', admin.site.urls),
     url(r'^playbooks/', include('ansible.urls')),
 ]
