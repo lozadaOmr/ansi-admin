d151bcb06ca18a17e27c2e5a5a747418fdee0e80
Omar Lozada <omrlozada@gmail.com>
2017-06-29 17:16:14 +0800

    Prevent 'create' uri be read as detail view params

--- src/ansible/urls.py
+++ src/ansible/urls.py
@@ -3,7 +3,7 @@ from ansible.forms import AnsibleForm1, AnsibleForm2
 from ansible.views import PlaybookWizard, PlaybookListView, PlaybookDetailView
 
 urlpatterns = [
+    url(r'^create/$', PlaybookWizard.as_view([AnsibleForm1, AnsibleForm2])),
     url(r'^$', PlaybookListView.as_view(), name='playbook-list'),
-    url(r'^(?P<pk>[-\w]+)/$', PlaybookDetailView.as_view(), name='playbook-detail'),
-    url(r'^create/$', PlaybookWizard.as_view([AnsibleForm1, AnsibleForm2]))
+    url(r'^(?P<pk>[-\w]+)/$', PlaybookDetailView.as_view(), name='playbook-detail')
 ]
