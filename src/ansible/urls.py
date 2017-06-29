4376beef0515270e275b0d37058826d71596d5d5
Omar Lozada <omrlozada@gmail.com>
2017-06-29 18:47:33 +0800

    change format style

--- src/ansible/urls.py
+++ src/ansible/urls.py
@@ -1,9 +1,13 @@
 from django.conf.urls import url
 from ansible.forms import AnsibleForm1, AnsibleForm2
-from ansible.views import PlaybookWizard, PlaybookListView, PlaybookDetailView
+from ansible.views import (
+    PlaybookWizard, PlaybookListView, PlaybookDetailView,
+)
+from . import views
+
 
 urlpatterns = [
     url(r'^create/$', PlaybookWizard.as_view([AnsibleForm1, AnsibleForm2])),
     url(r'^$', PlaybookListView.as_view(), name='playbook-list'),
-    url(r'^(?P<pk>[-\w]+)/$', PlaybookDetailView.as_view(), name='playbook-detail')
+    url(r'^(?P<pk>[-\w]+)/$', PlaybookDetailView.as_view(), name='playbook-detail'),
 ]
