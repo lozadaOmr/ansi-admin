04eeba1d9677104638c4bf18b132f8ed7b1393e3
Omar Lozada <omrlozada@gmail.com>
2017-07-01 23:46:54 +0800

    Add playbook file detail view

--- src/ansible/urls.py
+++ src/ansible/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 from ansible.forms import AnsibleForm1, AnsibleForm2
 from ansible.views import (
     PlaybookWizard, PlaybookListView, PlaybookDetailView,
+    PlaybookFileView
 )
 from . import views
 
@@ -9,5 +10,10 @@ from . import views
 urlpatterns = [
     url(r'^create/$', PlaybookWizard.as_view([AnsibleForm1, AnsibleForm2])),
     url(r'^$', PlaybookListView.as_view(), name='playbook-list'),
-    url(r'^(?P<pk>[-\w]+)/$', PlaybookDetailView.as_view(), name='playbook-detail'),
+    url(r'^(?P<pk>[-\w]+)/$',
+        PlaybookDetailView.as_view(), name='playbook-detail'
+    ),
+    url(r'^(?P<pk>[-\w]+)/files/(?P<slug>[\w-]+)/$',
+        PlaybookFileView.as_view(), name='playbook-file-detail'
+    ),
 ]
