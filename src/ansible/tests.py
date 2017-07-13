0c488f0c80cdf15fc58a6fc480740f8e7cd7dc6a
Omar Lozada <omrlozada@gmail.com>
2017-07-13 22:20:30 +0800

    View unit test

--- src/ansible/tests.py
+++ src/ansible/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from django.urls import reverse
+from .models import Playbook
 
-# Create your tests here.
+
+class QuestionViewTests(TestCase):
+    def test_index_view(self):
+        """
+        If no playbooks exist, an appropriate message should be displayed.
+        """
+        response = self.client.get(reverse('ansible:playbook-list'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "No playbooks yet.")
+        self.assertQuerysetEqual(response.context['latest_question_list'], [])
