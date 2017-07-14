4b96ecd6b17a3a4fb6f057b293b614aaba337e83
Omar Lozada <omrlozada@gmail.com>
2017-07-14 23:54:45 +0800

    Add tests_models.py

---
+++ src/ansible/tests/test_models.py
@@ -0,0 +1,20 @@
+from django.test import TestCase
+from ansible.models import Playbook
+
+class PlaybookModelTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        #Set up non-modified objects used by all test methods
+        Playbook.query_set.create(username='lozadaomr',repository='ansi-dst',
+                inventory='hosts',user='ubuntu')
+
+    def test_username_label(self):
+        playbook=Playbook.query_set.get(id=1)
+        field_label = playbook._meta.get_field('username').verbose_name
+        self.assertEquals(field_label,'username')
+
+    def test_username_max_length(self):
+        playbook=Playbook.query_set.get(id=1)
+        max_length = playbook._meta.get_field('username').max_length
+        self.assertEquals(max_length,39)
