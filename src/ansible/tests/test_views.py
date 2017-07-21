f04fdee164d45ff602bd515261f86e188bc7931d
Omar Lozada <omrlozada@gmail.com>
2017-07-21 22:15:57 +0800

    Remove trailing space

--- src/ansible/tests/test_views.py
+++ src/ansible/tests/test_views.py
@@ -9,9 +9,9 @@ class PlaybookListViewTest(TestCase):
         Playbook.query_set.create(username='lozadaomr',repository='ansi-dst',
                 inventory='hosts',user='ubuntu')
 
-    def test_view_url_exists_at_desired_location(self): 
-        resp = self.client.get('/playbooks/') 
-        self.assertEqual(resp.status_code, 200)  
+    def test_view_url_exists_at_desired_location(self):
+        resp = self.client.get('/playbooks/')
+        self.assertEqual(resp.status_code, 200)
 
     def test_view_playbook_list_url_accessible_by_name(self):
         resp = self.client.get(reverse('ansible:playbook-list'))
