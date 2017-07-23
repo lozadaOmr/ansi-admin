d66a5e06d7d3cb1f7642107308ef46032e239e55
Omar Lozada <omrlozada@gmail.com>
2017-07-23 16:15:17 +0800

    Remove ValidationTests

--- src/ansible/tests/test_utils.py
+++ src/ansible/tests/test_utils.py
@@ -1,29 +1,19 @@
 from django.test import TestCase
-from django.core.urlresolvers import reverse
-from django.core.validators import ValidationError
 from django.conf import settings
-from ansible.models import Playbook
 import utils.repository as utils
 import utils.slugify as slugify
-import os
-import shutil
+import mock
 
 
 class UtilsRepositoryTest(TestCase):
 
     def setUp(self):
-        self.playbook = Playbook.query_set.get(id=1)
-
-    @classmethod
-    def setUpTestData(cls):
-        Playbook.query_set.create(username='lozadaomr',repository='ansi-dst',
-                inventory='hosts',user='ubuntu')
-
-    def test_clone_repository(self):
-        utils.clone_repository(self.playbook.username, self.playbook.repository)
-        repo_path = os.path.exists(
-                os.path.join(settings.PLAYBOOK_DIR, self.playbook.repository))
-        self.assertTrue(repo_path)
+        self.playbook = {
+            'username' : 'lozadaomr',
+            'repository' : 'ansi-dst',
+            'hosts' : 'hosts',
+            'user' : 'ubuntu',
+        }
 
     # TODO: Find a way to mock this during test
     def test_generate_command_blank_data(self):
@@ -36,22 +26,13 @@ class UtilsRepositoryTest(TestCase):
         self.assertEqual(cmd, 'ansible-playbook test.yml')
 
     def test_get_dir_name(self):
-        repository_path = utils.get_dir_name(self.playbook.repository)
+        repository_path = utils.get_dir_name(self.playbook['repository'])
         self.assertEqual(repository_path, '/opt/app/playbooks/ansi-dst')
 
     def test_get_remote_repo_url(self):
-        repo_url = utils.get_remote_repo_url(self.playbook.username,
-                self.playbook.repository)
+        repo_url = utils.get_remote_repo_url(self.playbook['username'],
+                self.playbook['repository'])
         self.assertEqual(repo_url, 'https://github.com/lozadaomr/ansi-dst.git')
-
-    def test_validate_repository_raise_validation_error(self):
-        with self.assertRaises(ValidationError):
-            utils.validate_repository(self.playbook.repository)
-
-    def test_validate_inventory_raise_validation_error(self):
-        with self.assertRaises(ValidationError):
-            utils.validate_inventory(self.playbook.repository,
-                    '/opt/app/playbooks/ansi-dst/none.yml')
 
 
 class UtilsSlugifyTest(TestCase):
