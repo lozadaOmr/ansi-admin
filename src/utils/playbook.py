5b3c3a40a337227ecf7103292526512667e32f85
Omar Lozada <omrlozada@gmail.com>
2017-08-13 02:00:41 +0800

    Add utility file for playbook

---
+++ src/utils/playbook.py
@@ -0,0 +1,11 @@
+from django.conf import settings
+from ansible.models import Playbook
+import os
+
+
+def content_loader(pk, slug):
+    playbook = Playbook.query_set.get(pk=pk)
+    playbook_dir = playbook.directory
+    # TODO: for now assume without validation
+    playbook_file = os.path.join(playbook_dir, slug + '.yml')
+    return playbook_file
