533ed337dd5f6087ac059ed47f1a0ce344ce48ce
Omar Lozada <omrlozada@gmail.com>
2017-08-19 19:41:19 +0800

    Add util func to write content

--- src/utils/playbook.py
+++ src/utils/playbook.py
@@ -8,8 +8,17 @@ def content_loader(pk, slug):
     playbook_dir = playbook.directory
     # TODO: for now assume without validation
     playbook_file = os.path.join(playbook_dir, slug + '.yml')
-
     with open(playbook_file, 'r') as f:
         content = f.read()
-
     return content
+
+
+def write_content(pk, slug, data):
+   playbook = Playbook.query_set.get(pk=pk)
+
+   #TODO: assume for now file ends with '.yml'
+   playbook_file = slug + '.yml'
+   playbook_file = os.path.join(playbook.get_dir_name(), playbook_file)
+   f = open(playbook_file, "w")
+   f.write(data)
+   f.close
