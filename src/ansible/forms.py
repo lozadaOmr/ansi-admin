72fac582266f34212f533f15a3f1ccaaa6e00392
Omar Lozada <omrlozada@gmail.com>
2017-08-25 21:39:22 +0800

    Use clean_filename to validate if filename is already used

--- src/ansible/forms.py
+++ src/ansible/forms.py
@@ -1,7 +1,10 @@
 from django import forms
+from django.core.validators import ValidationError
 from django.conf import settings
 from django.forms import ModelForm
 from ansible.models import Playbook
+import utils.playbook as playbook_utils
+import os
 
 class AnsibleForm1(ModelForm):
     class Meta:
@@ -23,3 +26,16 @@ class LoginForm(forms.Form):
 class PlaybookFileForm(forms.Form):
     filename = forms.CharField(label='Filename', max_length=100)
     playbook = forms.CharField(widget=forms.Textarea(attrs={'rows':30,'cols':80}))
+
+    def __init__(self, *args, **kwargs):
+        self.pk = kwargs.pop('pk', None)
+        super(PlaybookFileForm, self).__init__(*args, **kwargs)
+
+    def clean_filename(self):
+        data = playbook_utils.append_extension(self.cleaned_data['filename'])
+        playbook = Playbook.query_set.get(pk=self.pk)
+        playbook_dir = playbook.directory
+        playbook_file_path = os.path.join(playbook_dir, data)
+        if os.path.exists(playbook_file_path):
+            raise forms.ValidationError("Filename already used")
+        return data
