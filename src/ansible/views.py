dd943042546dedb0f7853a3baae712e6069de344
Omar Lozada <omrlozada@gmail.com>
2017-08-30 20:48:37 +0800

    Use Python PEP8 on ansible views.py

--- src/ansible/views.py
+++ src/ansible/views.py
@@ -1,23 +1,24 @@
-from django.core.urlresolvers import reverse
-from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponseRedirect, HttpResponse
-from django.contrib.auth.decorators import login_required
-from django.contrib.auth import authenticate, login
-from django.views import View
-from django.views.generic.detail import DetailView
-from django.views.generic.list import ListView
-from formtools.wizard.views import SessionWizardView
-from ansible.models import Playbook
-from .forms import LoginForm, PlaybookFileForm
-import utils.repository as utils
-import utils.playbook as playbook_utils
 import os
 import subprocess
 import sys
 
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+from django.core.urlresolvers import reverse
+from django.views import View
+from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
+from django.http import HttpResponse, HttpResponseRedirect
+from formtools.wizard.views import SessionWizardView
+
+from .models import Playbook
+from .forms import LoginForm, PlaybookFileForm
+import utils.repository as utils
+import utils.playbook as playbook_utils
+
 
 def index(request):
-    return HttpResponse("200")
+    return HttpResponse('200')
 
 
 class Login(View):
@@ -42,22 +43,25 @@ class PlaybookWizard(SessionWizardView):
             result['prev_data'] = {}
             result['prev_data']['repository'] = prev_data['repository']
             return result
+
         return self.initial_dict.get(step, {})
 
     def get_form_step_data(self, form):
         if self.get_form_prefix() == '0':
-            self.prev_form_data['repository'] = form.data.dict()['0-repository']
+            self.prev_form_data['repository'] = (
+                form.data.dict()['0-repository']
+            )
             self.prev_form_data['username'] = form.data.dict()['0-username']
 
         if self.get_form_prefix() == '1':
             utils.validate_repository(self.prev_form_data['repository'])
             utils.clone_repository(
-                    self.prev_form_data['username'],
-                    self.prev_form_data['repository']
+                self.prev_form_data['username'],
+                self.prev_form_data['repository']
             )
             utils.validate_inventory(
-                    self.prev_form_data['repository'],
-                    form.data.dict()['1-inventory']
+                self.prev_form_data['repository'],
+                form.data.dict()['1-inventory']
             )
             playbook = Playbook()
             playbook.username = self.prev_form_data['username']
@@ -65,6 +69,7 @@ class PlaybookWizard(SessionWizardView):
             playbook.inventory = form.data.dict()['1-inventory']
             playbook.user = form.data.dict()['1-user']
             playbook.save()
+
         return form.data
 
     def done(self, form_list, form_dict, **kwargs):
@@ -87,48 +92,55 @@ class PlaybookDetailView(DetailView):
         return context
 
     def post(self, request, *args, **kwargs):
-        data = request.POST.get("playbook_file")
+        data = request.POST.get('playbook_file')
         current_dir = os.path.dirname(data)
         cmd = utils.generate_command(data)
-        proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True,
-                cwd=current_dir)
+        proc = subprocess.Popen(
+            cmd,
+            stdout=subprocess.PIPE,
+            shell=True,
+            cwd=current_dir
+        )
         while True:
             line = proc.stdout.readline()
             if line != '':
                 print line
             else:
                 break
-        proc.wait()
 
+        proc.wait()
         result = proc.stdout.read()
         return HttpResponse(result)
 
 
 class PlaybookFileView(View):
-    template_name = "ansible/playbookfile_detail.html"
+    template_name = 'ansible/playbookfile_detail.html'
 
     def get(self, request, *args, **kwargs):
         content = playbook_utils.content_loader(
-                self.kwargs['pk'], self.kwargs['slug']
+            self.kwargs['pk'], self.kwargs['slug']
         )
         return HttpResponse(content, content_type='text/plain')
 
 
 class PlaybookFileEditView(View):
     form_class = PlaybookFileForm
-    template_name = "ansible/playbookfile_edit.html"
+    template_name = 'ansible/playbookfile_edit.html'
 
     def get(self, request, *args, **kwargs):
         # TODO: Util to get playbook filename
         content = playbook_utils.content_loader(
-                self.kwargs['pk'], self.kwargs['slug']
+            self.kwargs['pk'], self.kwargs['slug']
         )
         form = self.form_class(initial={'playbook': content})
+
         return render(request, self.template_name, {
-            'form': form, 'pk': kwargs['pk'], 'slug': kwargs['slug']})
+            'form': form, 'pk': kwargs['pk'], 'slug': kwargs['slug']}
+        )
 
     def post(self, request, *args, **kwargs):
         form = self.form_class(request.POST)
+
         if form.is_valid():
             data = form.cleaned_data['playbook']
             playbook_utils.write_content(
@@ -139,24 +151,29 @@ class PlaybookFileEditView(View):
 
 class PlaybookFileCreateView(View):
     form_class = PlaybookFileForm
-    template_name = "ansible/playbookfile_create.html"
+    template_name = 'ansible/playbookfile_create.html'
 
     def get(self, request, *args, **kwargs):
         form = self.form_class()
         return render(request, self.template_name, {
-            'form': form, 'pk': self.kwargs['pk']})
+            'form': form, 'pk': self.kwargs['pk']}
+        )
 
     def post(self, request, *args, **kwargs):
         form = self.form_class(request.POST, pk=self.kwargs['pk'])
+
         if form.is_valid():
             data = form.cleaned_data['playbook']
             filename = form.cleaned_data['filename']
             playbook = Playbook.query_set.get(pk=self.kwargs['pk'])
             playbook_dir = playbook.directory
             playbook_file_path = os.path.join(playbook_dir, filename)
+
             # TODO: Add proper validation
             if not os.path.exists(playbook_file_path):
                 playbook_utils.create_playbook(playbook_file_path, data)
-            return HttpResponse("200")
+            return HttpResponse('200')
+
         return render(request, self.template_name, {
-            'form':form, 'pk': self.kwargs['pk']})
+            'form': form, 'pk': self.kwargs['pk']}
+        )
