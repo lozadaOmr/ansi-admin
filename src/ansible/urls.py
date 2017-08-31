9ba5ecdbd7ca3a95f5438346d3de773a6ac47d45
Omar Lozada <omrlozada@gmail.com>
2017-08-31 23:21:53 +0800

    Align with opening delimeter

--- src/ansible/urls.py
+++ src/ansible/urls.py
@@ -11,15 +11,11 @@ urlpatterns = [
     url(r'^create/$', PlaybookWizard.as_view([AnsibleForm1, AnsibleForm2])),
     url(r'^$', PlaybookListView.as_view(), name='playbook-list'),
     url(r'^(?P<pk>[-\w]+)/$',
-        PlaybookDetailView.as_view(), name='playbook-detail'
-    ),
+        PlaybookDetailView.as_view(), name='playbook-detail'),
     url(r'^(?P<pk>[-\w]+)/new/$',
-        PlaybookFileCreateView.as_view(), name='playbook-file-create'
-    ),
+        PlaybookFileCreateView.as_view(), name='playbook-file-create'),
     url(r'^(?P<pk>[-\w]+)/files/(?P<slug>[\w-]+)$',
-        PlaybookFileView.as_view(), name='playbook-file-detail'
-    ),
+        PlaybookFileView.as_view(), name='playbook-file-detail'),
     url(r'^(?P<pk>[-\w]+)/files/(?P<slug>[\w-]+)/edit$',
-        PlaybookFileEditView.as_view(), name='playbook-file-edit'
-    ),
+        PlaybookFileEditView.as_view(), name='playbook-file-edit'),
 ]
