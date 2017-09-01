de1caa47b9ce287f05e5b471f1e01a7b6ae98b66
Omar Lozada <omrlozada@gmail.com>
2017-09-01 22:31:41 +0800

    Make URLconf into PEP8

--- src/web/urls.py
+++ src/web/urls.py
@@ -6,7 +6,8 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     url(r'^login/', auth_views.login, name='login',
         kwargs={'redirect_authenticated_user': True}),
-    url(r'^logout/', auth_views.logout, {'next_page': '/login'}, name='logout'),
+    url(r'^logout/', auth_views.logout,
+        {'next_page': '/login'}, name='logout'),
     url(r'^admin/', admin.site.urls),
     url(r'^playbooks/', include('ansible.urls', namespace='ansible')),
 ]
