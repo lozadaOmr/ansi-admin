65e330d148aabe773f2524635e848bdc9a10b226
Omar Lozada <omrlozada@gmail.com>
2017-09-09 22:17:29 +0800

    Vertical aligned indention in ansible urls.py

--- src/ansible/urls.py
+++ src/ansible/urls.py
@@ -8,15 +8,37 @@ from ansible.views import (
 
 
 urlpatterns = [
-    url(r'^create/$', PlaybookWizard.as_view([AnsibleForm1, AnsibleForm2]),
-                                             name='playbook-create'),
-    url(r'^$', PlaybookListView.as_view(), name='playbook-list'),
-    url(r'^(?P<pk>[-\w]+)/$',
-        PlaybookDetailView.as_view(), name='playbook-detail'),
-    url(r'^(?P<pk>[-\w]+)/new/$',
-        PlaybookFileCreateView.as_view(), name='playbook-file-create'),
-    url(r'^(?P<pk>[-\w]+)/files/(?P<slug>[\w-]+)$',
-        PlaybookFileView.as_view(), name='playbook-file-detail'),
-    url(r'^(?P<pk>[-\w]+)/files/(?P<slug>[\w-]+)/edit$',
-        PlaybookFileEditView.as_view(), name='playbook-file-edit'),
+    url(
+        r'^create/$',
+        PlaybookWizard.as_view([
+            AnsibleForm1,
+            AnsibleForm2
+        ]),
+        name='playbook-create'
+    ),
+    url(
+        r'^$',
+        PlaybookListView.as_view(),
+        name='playbook-list'
+    ),
+    url(
+        r'^(?P<pk>[-\w]+)/$',
+        PlaybookDetailView.as_view(),
+        name='playbook-detail'
+    ),
+    url(
+        r'^(?P<pk>[-\w]+)/new/$',
+        PlaybookFileCreateView.as_view(),
+        name='playbook-file-create'
+    ),
+    url(
+        r'^(?P<pk>[-\w]+)/files/(?P<slug>[\w-]+)$',
+        PlaybookFileView.as_view(),
+        name='playbook-file-detail'
+    ),
+    url(
+        r'^(?P<pk>[-\w]+)/files/(?P<slug>[\w-]+)/edit$',
+        PlaybookFileEditView.as_view(),
+        name='playbook-file-edit'
+    ),
 ]
